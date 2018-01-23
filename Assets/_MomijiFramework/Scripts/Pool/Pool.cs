ecfac1c031dcf4b875f1ef94752c27557c897bb9
MizoTake <mizotake123@gmail.com>
2018-01-24 01:14:27 +0900

    add namespace

--- Assets/_MomijiFramework/Scripts/Pool/Pool.cs
+++ Assets/_MomijiFramework/Scripts/Pool/Pool.cs
@@ -3,23 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 using UniRx.Toolkit;
 
-public class Pool<T> : ObjectPool<T> where T : UnityEngine.Component
+
+namespace Momiji
 {
-
-    protected readonly T _prefab;
-    protected readonly Transform _parent;
-
-    public Pool(T prefab, Transform parent)
+    public class Pool<T> : ObjectPool<T> where T : UnityEngine.Component
     {
-        _parent = parent;
-        _prefab = prefab;
-    }
 
-    protected override T CreateInstance()
-    {
-        var newObj = GameObject.Instantiate(_prefab);
-        newObj.transform.SetParent(_parent);
-        newObj.transform.localScale = Vector3.one;
-        return newObj;
+        protected readonly T _prefab;
+        protected readonly Transform _parent;
+
+        public Pool(T prefab, Transform parent)
+        {
+            _parent = parent;
+            _prefab = prefab;
+        }
+
+        protected override T CreateInstance()
+        {
+            var newObj = GameObject.Instantiate(_prefab);
+            newObj.transform.SetParent(_parent);
+            newObj.transform.localScale = Vector3.one;
+            return newObj;
+        }
     }
 }
