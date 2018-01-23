5f83a18b7db88fad2ea3f6702123ea1853d33fee
MizoTake <mizotake123@gmail.com>
2018-01-24 01:18:01 +0900

    add namespace

--- Assets/_MomijiFramework/Scripts/UI/ScrollView/Base/TableView.cs
+++ Assets/_MomijiFramework/Scripts/UI/ScrollView/Base/TableView.cs
@@ -2,16 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class TableView<T> : MonoBehaviour where T : UnityEngine.Component
+namespace Momiji
 {
-    [SerializeField]
-    protected GameObject _cellPrefab;
-    protected Pool<T> _pool;
-    protected List<T> _cells = new List<T>();
+    public abstract class TableView<T> : MonoBehaviour where T : UnityEngine.Component
+    {
+        [SerializeField]
+        protected GameObject _cellPrefab;
+        protected Pool<T> _pool;
+        protected List<T> _cells = new List<T>();
 
-    public IReadOnlyList<T> Cell { get { return _cells; } }
+        public IReadOnlyList<T> Cell { get { return _cells; } }
 
-    public abstract int CellCount();
-    public abstract void ReloadData();
-    public abstract void TableViewCell(int index, T cell);
+        public abstract int CellCount();
+        public abstract void ReloadData();
+        public abstract void TableViewCell(int index, T cell);
+    }
 }
