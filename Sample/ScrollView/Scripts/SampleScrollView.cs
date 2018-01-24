caeeb6a560204f7745900cca107004db349b87aa
MizoTake <mizotake123@gmail.com>
2018-01-25 02:14:58 +0900

    sample

---
+++ Sample/ScrollView/Scripts/SampleScrollView.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Momiji;
+
+public class SampleScrollView : BaseTableView<SampleCell>
+{
+
+    void Start()
+    {
+        base.Start();
+        ReloadData();
+    }
+
+    public override int CellCount()
+    {
+        return 15;
+    }
+
+    public override void TableViewCell(int index, SampleCell cell)
+    {
+        cell.Config(new SampleCellParameter(index));
+    }
+}
