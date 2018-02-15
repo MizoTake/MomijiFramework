fe8ba8819a7244f0a397cf2f26bae93b4ee99d66
MizoTake <mizotake123@gmail.com>
2018-02-16 02:00:54 +0900

    add easy trans scene script

---
+++ Scripts/Behaviour/NextScene.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Momiji;
+
+public class NextScene : MonoBehaviour
+{
+
+    public SceneInfo.SceneEnum to;
+
+    public void Next()
+    {
+        TransSceneManager.LoadScene(to);
+    }
+}
