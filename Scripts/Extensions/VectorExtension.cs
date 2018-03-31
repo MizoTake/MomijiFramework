4fcfd809b2aea16d7af255abcb54c07ea2dc7406
MizoTake <mizotake123@gmail.com>
2018-03-31 09:28:16 +0900

    update vector

--- Scripts/Extensions/VectorExtension.cs
+++ Scripts/Extensions/VectorExtension.cs
@@ -6,27 +6,47 @@ namespace Momiji
 {
     public static class VectorExtension
     {
-
         public static Vector3 EnumToNormalize(this Vector selectNor, bool random = false)
         {
-            if (random) { }
-            switch (selectNor)
+            var count = 1;
+            var result = Vector3.zero;
+            var normalizes = new List<Vector> { selectNor };
+            if (random)
             {
-                case Vector.Up:
-                    return Vector3.up;
-                case Vector.Down:
-                    return Vector3.down;
-                case Vector.Left:
-                    return Vector3.left;
-                case Vector.Right:
-                    return Vector3.right;
-                case Vector.Forward:
-                    return Vector3.forward;
-                case Vector.Back:
-                    return Vector3.back;
-                default:
-                    return Vector3.zero;
+                count = Random.Range(1, 4);
             }
+            for (int i = 0; i < count; i++)
+            {
+                if (i > 0)
+                {
+                    normalizes.Add(Enum<Vector>.Random);
+                }
+                switch (normalizes[i])
+                {
+                    case Vector.Up:
+                        result += Vector3.up;
+                        break;
+                    case Vector.Down:
+                        result += Vector3.down;
+                        break;
+                    case Vector.Left:
+                        result += Vector3.left;
+                        break;
+                    case Vector.Right:
+                        result += Vector3.right;
+                        break;
+                    case Vector.Forward:
+                        result += Vector3.forward;
+                        break;
+                    case Vector.Back:
+                        result += Vector3.back;
+                        break;
+                    default:
+                        result += Vector3.zero;
+                        break;
+                }
+            }
+            return result;
         }
 
     }
