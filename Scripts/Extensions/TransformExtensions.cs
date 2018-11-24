8ec36b3824fa3a2f59e84d66838aba78e7d616e2
MizoTake <mizotake123@gmail.com>
2018-11-25 02:58:39 +0900

    add lerpmove

---
+++ Scripts/Extensions/TransformExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class TransformExtensions
+{
+
+	public static void LerpMove (this Transform transform, Vector3 targetPos, float speed)
+	{
+		var direction = (targetPos - transform.position).normalized;
+		transform.LookAt (new Vector3 (targetPos.x, transform.position.y, targetPos.z));
+		transform.Translate (direction * speed);
+	}
+}
