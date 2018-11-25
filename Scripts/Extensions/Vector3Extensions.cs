1fab382194ae9f41b5c1d15d8fe52602bda6df2c
MizoTake <mizotake123@gmail.com>
2018-11-25 15:05:42 +0900

    add option

--- Scripts/Extensions/Vector3Extensions.cs
+++ Scripts/Extensions/Vector3Extensions.cs
@@ -7,10 +7,26 @@ namespace Momiji
     public static class Vector3Extension
     {
 
-        public static Vector3 RandomRange(this Vector3 vec, float min, float max)
+        public static Vector3 RandomRange (this Vector3 vec, float min, float max)
         {
-            var value = Random.Range(min, max);
-            return new Vector3(value, value, value);
+            var value = Random.Range (min, max);
+            return new Vector3 (value, value, value);
+        }
+
+        public static Vector3 RandomX (this Vector3 vec, float min, float max)
+        {
+            var value = Random.Range (min, max);
+            return new Vector3 (value, vec.y, vec.z);
+        }
+        public static Vector3 RandomY (this Vector3 vec, float min, float max)
+        {
+            var value = Random.Range (min, max);
+            return new Vector3 (vec.x, value, vec.z);
+        }
+        public static Vector3 RandomZ (this Vector3 vec, float min, float max)
+        {
+            var value = Random.Range (min, max);
+            return new Vector3 (vec.x, vec.y, value);
         }
     }
 }
